1c96e8e43221144d6543ac9efb46817bd6925b4f
slyrackgo <aibragimov2003@bk.ru>
2025-03-06 22:42:51 +0100

    JPQL added

--- uni_library/src/main/java/com/example/uni_library/ProductRepository.java
+++ uni_library/src/main/java/com/example/uni_library/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.uni_library;
 
 import com.example.uni_library.Product.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,9 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     //spring data jpa
     List<Product> findByNameContaining(String name);
+
+
+    //JPQL
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR  p.description LIKE %:keyword%")
+    List<Product> findbyNameOrDescriptionContaining(@Param("keyword") String name);
 }
